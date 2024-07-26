4c77ce82200daba2b2fab386c238f9bb844287ea
Ashokrm26 <rashokise2023@gmail.com>
2024-07-26 16:22:18 +0530

    created an Placement.jsx file and added component in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Attendance from "./Attendance";
 import Classes from "./Classes";
+import Placements from './Placements';
 
 function App() {
   return (
     <>
       <Attendance />
       <Classes />
+      <Placements />
     </>
   );
 }
